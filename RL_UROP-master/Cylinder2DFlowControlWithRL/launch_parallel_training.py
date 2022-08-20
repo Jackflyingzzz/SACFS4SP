85a63004e0564097d0c5beb30283d69f66ade92e
Jackflyingzzz <105610300+Jackflyingzzz@users.noreply.github.com>
2022-08-20 15:18:02 +0100

    Update launch_parallel_training.py

--- RL_UROP-master/Cylinder2DFlowControlWithRL/launch_parallel_training.py
+++ RL_UROP-master/Cylinder2DFlowControlWithRL/launch_parallel_training.py
@@ -36,15 +36,11 @@ if __name__ == '__main__':
 
 
     config = {}
+
     config["learning_rate"] = 3e-4
     config["learning_starts"] = 0
     config["batch_size"] = 128
-    config["top_quantiles_to_drop_per_net"] = 3
-    config["policy_kwargs"] = {
-                                "n_critics": 5,
-                                "n_quantiles": 25,
-                                "net_arch": dict(pi=[256, 256], qf=[512, 512, 512])
-                                }
+
     config["tau"] = 5e-3
     config["gamma"] = 0.99
     config["train_freq"] = 1
@@ -56,13 +52,14 @@ if __name__ == '__main__':
 
     config["ent_coef"] = "auto_0.01"
     config["target_entropy"] = "auto"
+    policy_kwargs = dict(net_arch=dict(pi=[512, 512], qf=[1024, 1024, 1024]))
     checkpoint_callback = CheckpointCallback(
-                                            save_freq=max(10, 1),
+                                            save_freq=max(5, 1),
                                             #num_to_keep=5,
                                             #save_buffer=True,
                                             #save_env_stats=True,
                                             save_path=savedir,
-                                            name_prefix='TQC2SP30FS_model')
+                                            name_prefix='TQC4SP30FS_model')
 
 
     env = SubprocVecEnv([resume_env(nb_actuations,i) for i in range(number_servers)], start_method='spawn')
